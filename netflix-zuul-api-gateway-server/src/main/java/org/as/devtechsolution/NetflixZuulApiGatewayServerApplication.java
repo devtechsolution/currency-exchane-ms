d2f6ef5bc42d302475f8934c720eabf86926cbdf
devtechsolution <srivastvadevzone@gmail.com>
2019-11-24 17:12:39 +0530

    Setting up Zuul API Gateway

---
+++ netflix-zuul-api-gateway-server/src/main/java/org/as/devtechsolution/NetflixZuulApiGatewayServerApplication.java
@@ -0,0 +1,15 @@
+package org.as.devtechsolution;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class NetflixZuulApiGatewayServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetflixZuulApiGatewayServerApplication.class, args);
+	}
+
+}
